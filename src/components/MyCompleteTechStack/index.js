7faf82c334eddb660e3930f83208c35506c24aeb
Gloria Giannascoli <g.giannascoli@reply.it>
2021-03-06 22:45:46 +0100

    added tech stack section

--- src/components/MyCompleteTechStack/index.js
+++ src/components/MyCompleteTechStack/index.js
@@ -7,21 +7,29 @@ export default function MyTechStack (){
 return <div className={style.wrapper}>
     <h2 className={style.stackTitle}>My Complete Tech Stack</h2>
     <div className={style.fe}>
-    <h3 className={style.feTitle}> Frontend</h3>
-    <div className={style.myImgWrapper}>
+    <h3 className={style.title}> Frontend</h3>
+    <div className={style.myFeImgWrapper}>
     <img className={style.myImg} src="react.png"></img>
-        <img className={style.myImg} src="Angular_full_color_logo.png"></img>
-        <img className={style.myImg} src="redux.png"></img>
-        <img className={style.myImg} src="HTML5_Badge_512.png"></img>
-        <img className={style.myImg} src="css3_logo.png"></img>
-        <img className={style.myImg} src="js-logo.png"></img>
+        <img className={style.myFeImg} src="Angular_full_color_logo.png"></img>
+        <img className={style.myFeImg} src="redux.png"></img>
+        <img className={style.myFeImg} src="HTML5_Badge_512.png"></img>
+        <img className={style.myFeImg} src="css3_logo.png"></img>
+        <img className={style.myFeImg} src="js-logo.png"></img>
         </div>
     </div>
     <div className={style.be}>
-
+    <h3 className={style.title}> Backend</h3>
+    <div className={style.myBeImgWrapper}>
+    <img className={style.myImg} src="nodejs.png"></img>
+    </div>
     </div>
     <div className={style.devOps}>
-
+    <h3 className={style.title}> DevOps</h3>
+    <div className={style.myDoImgWrapper}>
+    <img className={style.myDoImg} src="jira-1-svg.png"></img>
+    <img className={style.myDoImg} src="devops.png"></img>
+    <img className={style.myDoImg} src="gitlab.png"></img>
+    </div>
     </div>
 
 </div>
