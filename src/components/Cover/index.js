f9402c3ef09956f479acd8104b6ea3602bdf24cd
Gloria Giannascoli <g.giannascoli@libero.it>
2021-03-31 09:36:39 +0200

    wip

--- src/components/Cover/index.js
+++ src/components/Cover/index.js
@@ -8,7 +8,7 @@ export default function Cover () {
 return <div className={style.devFlex}>
         <div className={style.developer}>
             <h1 className={style.mainTitle}> It's me, Gloria <br /> I am a developer</h1>
-            <p> I create amazing web experiences </p>
+            <h3 className={style.coverh2}> I create amazing web experiences </h3>
         </div>
         <div className={style.imageDiv}>
         <img src="isometric-cover@2x.png" className={style.isoCover}/>
