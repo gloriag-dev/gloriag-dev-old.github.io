2a3f64e133e7b6628aec32109fd2e0489e7af1d3
Gloria Giannascoli <g.giannascoli@libero.it>
2021-04-13 11:56:48 +0200

    wip

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,8 +3,8 @@ import React from "react"
 import style from "./style.module.scss"
 
 export default function Footer () {
-    return <div classname={style.main}>
-        <div classname={style.dragonDiv}>
+    return <div className={style.main}>
+        <div className={style.dragonDiv}>
         <img className={style.dragon} src="Rainbow_Dragon.svg"></img>
         </div>
     </div>
