8c2554f930d1e4342fcdf63fc36f5c74ab97224c
gloriag-dev <64480994+gloriag-dev@users.noreply.github.com>
2021-04-17 19:31:17 +0000

    Deploying to gh-pages from @ gloriag-dev/gloriag-dev.github.io@44ffaffe6061e56fc2a3d68e21393fb1fa08c1d6 🚀

---
+++ _next/static/o5GBBIMV5zU0jW5xN1GuJ/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002Fdb7d6bd48a46d5dcc89b.css","static\u002Fchunks\u002Fpages\u002Findex-96a5fa4a657e5cd9e6b1.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-142ab8b9970bd0ad52b2.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
