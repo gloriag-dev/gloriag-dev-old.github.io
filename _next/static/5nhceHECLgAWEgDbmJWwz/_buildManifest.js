bb4c98d4c9105162589b50c61b1251a23a7aad3d
gloriag-dev <gloriag-dev@users.noreply.github.com>
2021-04-18 18:19:23 +0000

    deploy: b6be6f80458e21131642d4a1f45ede1eb732c62b

---
+++ _next/static/5nhceHECLgAWEgDbmJWwz/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fcss\u002F5b7316f71801ee3b4ab7.css","static\u002Fchunks\u002Fpages\u002Findex-bfc03463a224167fbf50.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-974bf66fec47b106a77e.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
