b6be6f80458e21131642d4a1f45ede1eb732c62b
Gloria Giannascoli <g.giannascoli@libero.it>
2021-04-18 20:18:38 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,13 @@ import WhatIDo from "../components/WhatIDo"
 import MyTechStack from "../components/MyCompleteTechStack"
 import ContactMeForm from "../components/ContactMeForm"
 import Footer from "../components/Footer"
-import Helmet from 'helmet'
 import Head from "next/head"
 
 export default function Home() {
    return (<div className={style.home}>
-   <Head>
-  <title>Gloria's Portfolio</title>
-</Head>
+      <Head>
+      <title>Gloria's Portfolio</title>
+      </Head>
       <Navbar />
        <Cover />
       <MyWorks />
